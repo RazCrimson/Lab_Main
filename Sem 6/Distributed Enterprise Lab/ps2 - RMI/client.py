d90196d5fe0fdfee4370200b24713048a5577810
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2021-12-31 17:56:40 +0530

    add: DECL - ps2: RMI with Pyro

---
+++ Sem 6/Distributed Enterprise Lab/ps2 - RMI/client.py
@@ -0,0 +1,63 @@
+import sys
+import Pyro4
+import Pyro4.util
+
+from interface import ElectorInterface
+
+sys.excepthook = Pyro4.util.excepthook
+
+elector: ElectorInterface = Pyro4.Proxy("PYRONAME:elector")
+
+while True:
+    print(
+        "1. Register to Vote\n"
+        "2. Verify Voter\n"
+        "3. Get Candidates Names\n"
+        "4. Vote\n"
+        "5. Tally Results\n"
+        "6. Announce Winner\n"
+        "Press anything else to Quit\n"
+    )
+    choice = input("Enter your choice: ")
+    match choice:
+        case "1":
+            name = input("Enter name to register: ")
+            voter_id = elector.register_voter(name)
+            print(f"Voter ID for {name}: {voter_id}")
+        
+        case "2":
+            voter_id = input("Enter voter ID to verify: ")
+            name = elector.verify_voter(voter_id)
+            if name:
+                print(f"Voter for {voter_id}: {name}")
+            else:
+                print("Invalid or unregistered Voter ID!")
+
+        case "3":
+            candidates = elector.get_candidates()
+            print("\nCandidates: ")
+            for candidate in candidates:
+                print(candidate)
+
+        case "4":
+            voter_id = input("Enter voter ID to vote as: ")
+            candidate = input("Enter candidate to vote: ")
+            result = elector.vote(voter_id, candidate)
+            if result:
+                print("Voting succeeded!")
+            else:
+                print("Voting Failed!")
+
+        case "5":
+            candidate_scores = elector.tally_results()
+            print("Voting Tally: ")
+            for candidate, score in candidate_scores.items():
+                print(candidate, " - ", score)
+
+        case "6":
+            print(f"Winner is {elector.announce_winner()}")
+
+        case _:
+            print("Exiting....")
+            exit()
+    print() # For visual comfort
---
+++ Sem 6/Distributed Enterprise Lab/ps2 - RMI/interface.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Optional, List
+
+class ElectorInterface(ABC):
+    @abstractmethod
+    def register_voter(self, name: str) -> str:
+        pass
+
+    @abstractmethod
+    def verify_voter(self, voter_id: str) -> Optional[str]:
+        pass
+
+    @abstractmethod
+    def get_candidates(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def vote(self, voter_id: str, candidate: str) -> bool:
+        pass
+
+    @abstractmethod
+    def tally_results(self) -> dict:
+        pass
+
+    @abstractmethod
+    def announce_winner(self) -> str:
+        pass
+
