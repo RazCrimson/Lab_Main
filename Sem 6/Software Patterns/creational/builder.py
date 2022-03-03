496893ca07adf4d3072816201ce8725c2ecb4786
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2022-03-03 11:02:34 +0530

    SP: Creational Patterns

---
+++ Sem 6/Software Patterns/creational/builder.py
@@ -0,0 +1,68 @@
+from typing import Tuple, List
+
+
+class URLBuilder:
+    def __init__(self):
+        self._scheme = None
+        self._domain = None
+        self._port = None
+        self._path = None
+        self._query_parameters = {}
+
+    def set_scheme(self, scheme: str):
+        self._scheme = scheme
+        return self
+
+    def set_domain(self, domain: str):
+        self._domain = domain
+        return self
+
+    def set_port(self, port: int):
+        self._port = port
+        return self
+
+    def set_path(self, path: str):
+        # This is a POC example. Process path segements with validations for RL cases
+        self._path = path
+        return self
+
+    def set_query_parameter(self, key: str, value: str):
+        self._query_parameters[key] = value
+        return self
+
+    def set_query_parameters(self, params: List[Tuple[str, str]]):
+        for key, value in params:
+            self._query_parameters[key] = value
+        return self
+
+    def build(self) -> str:
+        if not self._scheme:
+            raise ValueError("No scheme set")
+        if not self._domain:
+            raise ValueError("No domain set")
+        if not self._port:
+            raise ValueError("No port set")
+
+        url = f"{self._scheme}://{self._domain}:{self._port}/"
+
+        if self._path:
+            url += self._path
+        if self._query_parameters:
+            url += "?"
+
+        url += "&".join(f"{key}={value}" for key, value in self._query_parameters.items())
+
+        return url
+
+
+if __name__ == "__main__":
+    url = (
+        URLBuilder()
+        .set_scheme("http")
+        .set_domain("localhost")
+        .set_port(8000)
+        .set_path("random/path.html")
+        .set_query_parameter("test", "example")
+        .build()
+    )
+    print(url)
---
+++ Sem 6/Software Patterns/creational/prototype.py
@@ -0,0 +1,79 @@
+# Example 1 - Using a separate method
+from abc import ABC, abstractmethod
+import copy
+
+
+class Prototype(ABC):
+    @abstractmethod
+    def clone():
+        ...
+
+
+class Component(Prototype):
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def get_x_coordinate(self) -> int:
+        return self.x
+
+    def get_y_coordinate(self) -> int:
+        return self.y
+
+    def clone(self):
+        return self.__class__(self.x, self.y)
+
+    def __str__(self):
+        return f"<{self.__class__}: " + ", ".join(f"{key}='{getattr(key)}'" for key in dir(self))
+
+
+class Button(Component):
+    def __init__(self, x: int, y: int, length: int, breadth: int, text):
+        super().__init__(x, y)
+        self.length = length
+        self.breadth = breadth
+        self.button_text = text
+
+    def clone(self):
+        button_text = copy.deepcopy(self.button_text)
+        return self.__class__(self.x, self.y, self.length, self.breadth, button_text)
+
+
+component = Component(10, 11)
+print(component)
+print(component.clone())
+button = Button(12, 13, 5, 5, "Text")
+print(button)
+print(button.clone())
+
+
+# Example 2 - Using python dunder methods
+
+
+class ChildComponent(Component, ABC):
+    def __init__(self, x: int, y: int, parent: Component) -> None:
+        super().__init__(x, y)
+        self.parent = parent
+
+    def get_parent(self):
+        return self.parent
+
+    def __copy__(self):
+        return copy.copy(self)
+
+    def __deepcopy__(self):
+        parent = copy.copy(self.parent)
+        return self.__class__(self.x, self.y, parent)
+
+
+class ChildButton(ChildComponent):
+    def __init__(self, x, y, parent, length, breadth, text):
+        super().__init__(x, y, parent)
+        self.length = length
+        self.breadth = breadth
+        self.button_text = text
+
+    def clone(self):
+        parent = copy.copy(self.parent)
+        button_text = copy.deepcopy(self.button_text)
+        return self.__class__(self.x, self.y, parent, self.length, self.breadth, button_text)
