e973fb00cbd0df54952a5ca0b97ea9e3e77a69ba
Bharath Vignesh J K <52282402+RazCrimson@users.noreply.github.com>
2023-08-10 20:41:06 +0530

    add: PCD - ps2

---
+++ Sem 9/PCD/ps2/sample.c
@@ -0,0 +1,33 @@
+#include <ostream>  
+/*rando*/ 
+
+/* 
+
+Sample program to just test regex
+
+\*/
+
+
+using namespace std;  
+int main()  
+{  
+ char* s = "sadfdsf /* sdfsdf */"     ;
+int z = 23; /* declare and define
+variable in C++. */   int y = 32; /*comment*/
+ int x = 35; 
+// variable ouput    
+ cout<<x<<"\n";    
+ char* s = "sadfdsf /* sdfsdf */"     ;
+
+ float r = .14;
+        for(int i = 0; i < MAX; i++) {
+                printf("%d\n", i);
+                printf("%d//\n", i);
+        }
+
+}  
+/*rando*/
+
+"192.168.1.1"
+"00:0a:95:9d:67:16"
+"http://gogole.com"
